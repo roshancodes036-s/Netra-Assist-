30ec70e234f53cebc88a2765e8acaa02f0c0ab5d
roshancodes036-sudo <roshancodes036@gmail.com>
2026-01-07 15:53:50 +0000

    Added Final CodeNetra APK

--- lib/ai_logic.dart
+++ lib/ai_logic.dart
@@ -2,7 +2,7 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 
 class AIBrain {
   // ✅ आपकी API Key
-  static const String _apiKey = "AIzaSyCxct3Gu814nqGcQIhj5q3u-D0VJAuiA8Q";
+  static const String _apiKey = "AIzaSyB8pvagisvGUVIDS0LNzMW_uvEIg6BfAxA";
 
   late GenerativeModel _model;
   late ChatSession _chat;
@@ -11,13 +11,13 @@ class AIBrain {
   void initBrain() {
     try {
       _model = GenerativeModel(
-        // ✅ CHANGE: 'latest' लगाने से यह एरर फिक्स हो जाएगा
-        model: 'gemini-1.5-flash-latest',
+        // ✅ FIX: 'gemini-pro' एक स्थिर मॉडल है जो इस API वर्शन के साथ काम करेगा।
+        model: 'gemini-2.5-flash',
         apiKey: _apiKey,
       );
       _chat = _model.startChat();
       _isInitialized = true;
-      print("✅ CodeNetra Brain: ACTIVE (Gemini 1.5 Flash)");
+      print("✅ CodeNetra Brain: ACTIVE ('gemini-2.5-flash',)");
     } catch (e) {
       print("❌ Brain Error: $e");
     }
@@ -36,7 +36,5 @@ class AIBrain {
     }
   }
 
-  void stopSpeaking() {
-    print("Stopping voice...");
-  }
+  void stopSpeaking() {}
 }
